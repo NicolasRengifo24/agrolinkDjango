e692363190461930d0443a0fdc5b26365d748564
NicolasRengifo24 <nicolaseggo@gmail.com>
2026-03-30 14:27:07 -0500

    modulo transportista con funcionalidad del mapa , y el modal del mapa

--- envios/views.py
+++ envios/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Envio
 from productos.models import Producto,Finca
 from usuarios.models import Usuario
+import json
 
 # Create your views here.
 def inicio(request):
@@ -11,11 +12,23 @@ def inicio(request):
         "id_vehiculo",
         "id_transportista"
     ).all()
+    
+    data_envios = []
+
+    for envio in envios:
+        if envio.latitud_origen and envio.longitud_origen:
+            data_envios.append({
+                "id" : envio.id_envio,
+                "origen": [envio.latitud_origen, envio.longitud_origen],
+                "destino": [envio.latitud_destino, envio.longitud_destino],
+                "numero": envio.numero_seguimiento
+            })
 
 
 
     return render(request, 'envios/envios_dashboard.html',{
     'envios' : envios,
+    'envios_json' : json.dumps(data_envios),
 
     })
     
