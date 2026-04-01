5f56fd9c21a00a48f07610ff87b3145a8006b4ef
Samuel1000-90 <samueldg0210@gmail.com>
2026-04-01 00:07:11 -0500

    ca

--- calificaciones/models.py
+++ calificaciones/models.py
@@ -3,7 +3,7 @@ from productos.models import Producto
 from django.contrib.auth.models import User
 
 class Calificacion(models.Model):
-    producto = models.ForeignKey('productos.Producto', on_delete=models.CASCADE)
+    producto = models.ForeignKey('productos.Producto', on_delete=models.CASCADE, related_name='calificaciones')
     usuario = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     puntaje = models.IntegerField()
     comentario = models.TextField(blank=True, null=True)
